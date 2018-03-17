b0772f86e35928578b279bb5cc657dd34b5cc390
Igor Maguskin <maguskin.igor@gmail.com>
2018-03-17 03:59:12 +0000

    Add Koin startup to Application

--- src/main/kotlin/com/igorini/togepibot/Launcher.kt
+++ src/main/kotlin/com/igorini/togepibot/Launcher.kt
@@ -1,6 +1,13 @@
 package com.igorini.togepibot
 
-/** Represents a Launcher */
+import org.koin.dsl.module.applicationContext
+import org.koin.standalone.StandAloneContext.startKoin
+
+val TogepiBotModule = applicationContext {
+    bean { TogepiBot() }
+}
+
 fun main(args : Array<String>) {
-    TogepiBot().start()
+    startKoin(listOf(TogepiBotModule))
+    Application()
 }
