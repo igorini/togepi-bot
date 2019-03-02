b1f6e4dcdda01b09c6c8957c27c54994ea01f0f2
igorini <maguskin.igor@gmail.com>
2019-03-02 15:57:38 +0000

    Change sound volume of GintamaKeyword

--- src/main/kotlin/com/igorini/togepibot/gui/keyword/anime/GintamaKeyword.kt
+++ src/main/kotlin/com/igorini/togepibot/gui/keyword/anime/GintamaKeyword.kt
@@ -4,6 +4,9 @@ import com.igorini.togepibot.gui.keyword.Keyword
 
 /** Represents a keyword for text and sound associated with "Gintama" */
 object GintamaKeyword : Keyword() {
+    override val soundVolume: Double
+        get() = 0.02
+
     override fun folder() = "anime\\gintama"
     override fun voiceRus() = listOf("гинтама", "гинтаму", "гинтаме", "гинтамой")
     override fun voiceEng() = listOf("gintama")
